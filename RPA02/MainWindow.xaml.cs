c0eaf2677ac45370b4c8f038891674f080fa2551
tkanazawa <tkanazawadev@gmail.com>
2019-08-09 17:03:30 +0900

    stop

--- RPA02/MainWindow.xaml.cs
+++ RPA02/MainWindow.xaml.cs
@@ -30,6 +30,9 @@ namespace RPA02
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            scratch.Visibility = System.Windows.Visibility.Visible;
+            Environment.SetEnvironmentVariable("ITSRPA01STOP", string.Empty, EnvironmentVariableTarget.User);
+
             try
             {
                 ScriptOptions options = ScriptOptions.Default
@@ -55,6 +58,9 @@ namespace RPA02
             ScriptRobot.Text = ScriptRobot.Text + "System.Diagnostics.Process.Start(\"https://www.google.com/\");";
             ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
             ScriptRobot.Text = ScriptRobot.Text + "Thread.Sleep(" + "1000" + ");";
+
+            ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
+            ScriptRobot.Text = ScriptRobot.Text + "if (Environment.GetEnvironmentVariable(\"ITSRPA01STOP\", EnvironmentVariableTarget.User) == \"1\"){throw new System.ArgumentException(\" * *********ITSRPA01STOP * *********\");}";
         }
 
         private void ScriptRobot_TextChanged(object sender, TextChangedEventArgs e)
@@ -69,6 +75,9 @@ namespace RPA02
             ScriptRobot.Text = ScriptRobot.Text + "SendKeys.SendWait(\"" + s + "\");";
             ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
             ScriptRobot.Text = ScriptRobot.Text + "Thread.Sleep(" + "1000" + ");";
+
+            ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
+            ScriptRobot.Text = ScriptRobot.Text + "if (Environment.GetEnvironmentVariable(\"ITSRPA01STOP\", EnvironmentVariableTarget.User) == \"1\"){throw new System.ArgumentException(\" * *********ITSRPA01STOP * *********\");}";
         }
 
         private void InputEnter_Click(object sender, RoutedEventArgs e)
@@ -78,6 +87,9 @@ namespace RPA02
             ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
             ScriptRobot.Text = ScriptRobot.Text + "Thread.Sleep(" + "1000" + ");";
 
+            ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
+            ScriptRobot.Text = ScriptRobot.Text + "if (Environment.GetEnvironmentVariable(\"ITSRPA01STOP\", EnvironmentVariableTarget.User) == \"1\"){throw new System.ArgumentException(\" * *********ITSRPA01STOP * *********\");}";
+
         }
 
         private void StartProgram_Click(object sender, RoutedEventArgs e)
@@ -86,6 +98,40 @@ namespace RPA02
             ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
             ScriptRobot.Text = ScriptRobot.Text + "System.Diagnostics.Process p = System.Diagnostics.Process.Start(@\"" + s + "\");";
             ScriptRobot.Text = ScriptRobot.Text + "Thread.Sleep(" + "3000" + ");";
+
+            ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
+            ScriptRobot.Text = ScriptRobot.Text + "if (Environment.GetEnvironmentVariable(\"ITSRPA01STOP\", EnvironmentVariableTarget.User) == \"1\"){throw new System.ArgumentException(\" * *********ITSRPA01STOP * *********\");}";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (scratch.Visibility == System.Windows.Visibility.Visible)
+            {
+                scratch.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                scratch.Visibility = System.Windows.Visibility.Visible;
+            }
+
+            
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            
+            Environment.SetEnvironmentVariable("ITSRPA01STOP", "1", EnvironmentVariableTarget.User);
+            MessageBox.Show("RPAを停止しました", "停止");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            ScriptRobot.Text = ScriptRobot.Text + Environment.NewLine;
+            ScriptRobot.Text = ScriptRobot.Text + "if (Environment.GetEnvironmentVariable(\"ITSRPA01STOP\", EnvironmentVariableTarget.User) == \"1\"){throw new System.ArgumentException(\" * *********ITSRPA01STOP * *********\");}";
+            /*
+            var itsRPAStop = Environment.GetEnvironmentVariable("ITSRPA01STOP", EnvironmentVariableTarget.User);
+            if (itsRPAStop == "1"){throw new System.ArgumentException("**********ITSRPA01STOP**********");}
+            */
         }
     }
 }
